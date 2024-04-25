c6807bf8406bc13952d27d30c332e07545dbf9e9
eds20000 <158028600+eds20000@users.noreply.github.com>
2024-04-26 07:34:16 +0900

    changed

--- script/script.js
+++ script/script.js
@@ -4,22 +4,22 @@
 
 
 function sort__tab(a){
-    document.querySelector(".content__title-sort-list").innerHTML = a.innerHTML;
+    document.querySelector(".content__title-sort-selected").innerHTML = a.innerHTML;
        
 }
 
-var sort__tab_content = document.getElementsByClassName("content__title-sort-list");
+var sort__tab_content = document.getElementsByClassName("content__title-sort-selected");
 
 var sort__tab_list = document.getElementsByClassName('sort__tab-list');
 
-console.log(sort__tab_content.outerText);
+console.log(sort__tab_content.innerHTML);
 console.log(sort__tab_list);
 
 
 for (let i of sort__tab_list){
     console.log(i);
     if(i.innerHTML === sort__tab_content.innerHTML){
-        i.style.fontWeight ='600';
+        i.classList.add('tab_selected') ='600';
     }
 }
 //sort-tab
