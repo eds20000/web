a9a88f6d60727c7f8b0ec55268b5b359223496ce
eds20000 <158028600+eds20000@users.noreply.github.com>
2024-05-09 01:03:32 +0900

    changed509

--- script/script.js
+++ script/script.js
@@ -1,16 +1,20 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 //sort-tab start
 function sort__tab(a){
     document.querySelector(".content__title-sort-selected").value = a.innerHTML;
     var sorttab = document.querySelectorAll(".sort__tab-list");
-    for (let i of sorttab){
-                 if(i.innerHTML === document.querySelector(".content__title-sort-selected").value){
-                    i.classList.add("selected");
-                    }
-                else{
-                    i.classList.remove("selected");
+    document.querySelector('.sort__tab-list.selected').classList.remove('selected');
+    a.classList.add('selected');
+    // for (let i of sorttab){
+    //              if(i.innerHTML === document.querySelector(".content__title-sort-selected").value){
+    //                 i.classList.add("selected");
+    //                 }
+    //             else{
+    //                 i.classList.remove("selected");
 
-                    }
-                }
+    //                 }
+    //             }
             }
 // sort-tab end//
 
@@ -18,7 +22,7 @@ function sort__tab(a){
 
 function favoritebutton(a){
     console.log(a.classList[1])
-    if(a.classList[1]==='sort__item-takecart-disable'){
+    if(a.classList.contains('sort__item-takecart-disable')){
         a.classList.remove('sort__item-takecart-disable');
         a.classList.add('sort__item-takecart-enable');
     }
@@ -30,5 +34,88 @@ function favoritebutton(a){
 
 //item end//
 
+// <!---------------Page-bar--------Start--> 
+
+var pageButton = document.querySelectorAll(".bar-page_list-item");
+Array.from(pageButton).forEach(function(item){
+    item.addEventListener('click',function(){
+        document.querySelector('.page-checked').classList.remove('page-checked')
+        this.classList.add('page-checked');
+    });
+}
+
+);
 
 
+
+// <!---------------Page-bar--------End--> 
+
+
+
+// LOGIN-PAGE
+var input_userid = $('.login-form-input_userid')
+var input_password = $('.login-form-input_userpassword')
+var passHide=$('.login-form-input_userpassword')
+const ID_label = $('.login-id')
+const Pass_label = $('.login-password')
+
+document.body.onload = function(){
+    if (input_password.value !== '') {
+        Pass_label.classList.add('labelMove_focus');
+        Pass_label.classList.remove('labelMove_blur');
+    }
+    if (input_userid.value !== '') {
+        ID_label_label.classList.add('labelMove_focus');
+        ID_label_label.classList.remove('labelMove_blur');
+    } 
+}
+$('.pass-hide').onclick = function () {
+    if(this.classList.contains('hidden')){
+        passHide.type='text'
+        this.classList.remove('hidden');
+        this.classList.add('visible');
+        this.innerHTML = '非表示';
+    }else{
+        passHide.type='password'
+        this.classList.remove('visible');
+        this.classList.add('hidden');
+        this.innerHTML = '表示';
+
+    }
+
+}
+
+
+var labelid_focus = function(){
+    ID_label.classList.add('labelMove_focus')
+    ID_label.classList.remove('labelMove_blur')
+}
+
+var labelid_blur = function(){
+    if (input_userid.value === '') {
+        ID_label.classList.remove('labelMove_focus');
+        ID_label.classList.add('labelMove_blur');
+    }
+}
+
+var labelpass_focus = function(){
+    Pass_label.classList.add('labelMove_focus')
+    Pass_label.classList.remove('labelMove_blur')
+}
+
+var labelpass_blur = function(){
+    if (input_password.value === '') {
+        Pass_label.classList.remove('labelMove_focus');
+        Pass_label.classList.add('labelMove_blur');
+    }
+}
+input_userid.onchange = labelid_focus
+input_userid.onfocus = labelid_focus
+input_userid.onblur = labelid_blur
+
+input_password.onchange = labelpass_focus
+input_password.onfocus = labelpass_focus
+input_password.onblur = labelpass_blur
+
+
+// 
