ea65af95b15d56915ca9065f1a943592a7d6062c
23635014NGUEN THANH SON <23844108@edu.hagoromo.ac.jp>
2024-05-13 12:54:38 +0900

    change

--- script/script.js
+++ script/script.js
@@ -56,36 +56,37 @@ for(let i = 0 ; i< list_item.length ; ++i){
     sort__item_list.innerHTML =
     `<div class="col l-2-4">
         <div class="sort__item">
-        <a href="#" class="sort__item-link" >
-            <div class="sort__item-img">
-                <img src="${list_item[i].img[i]}" alt="">
-                <div class="sort__item-img_btn sort__item-img_btn-left"><i class="fa-solid fa-angle-left"></i></div>
-                <div class="sort__item-img_btn sort__item-img_btn-right"><i class="fa-solid fa-angle-right"></i></div>
-            </div>
-            <div class="sort__item-brand">${list_item[i].brand}</div>
-            <div class="sort__item-text">${list_item[i].name}</div>
-        </a>
-        <div class="sort__item-content">
-            <div class="sort__item-title">
-                <div class="sort__item-price">￥${list_item[i].price}</div>
-                <div class="sort__item-takeit">
-                    <button type="button" class="sort__item-favorite sort__item-takecart-disable" onclick="favoritebutton(this)"></button>
-                    <button type="button" class="sort__item-takecart"><i class="fa-solid fa-cart-plus"></i></button>
+            <a href="./product.html" class="sort__item-link" >
+                <div class="sort__item-img">
+                    <img src="${list_item[i].img[i]}" alt="">
+                    <div class="sort__item-img_btn sort__item-img_btn-left"><i class="fa-solid fa-angle-left"></i></div>
+                    <div class="sort__item-img_btn sort__item-img_btn-right"><i class="fa-solid fa-angle-right"></i></div>
+                </div>
+                <div class="sort__item-brand">${list_item[i].brand}</div>
+                <div class="sort__item-text">${list_item[i].name}</div>
+            </a>
+            <div class="sort__item-content">
+                <div class="sort__item-title">
+                    <div class="sort__item-price">￥${list_item[i].price}</div>
+                    <div class="sort__item-takeit">
+                        <button type="button" class="sort__item-favorite sort__item-takecart-disable" onclick="favoritebutton(this)"></button>
+                        <button type="button" class="sort__item-takecart"><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                </div>
+                <div class="sort__item-star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <div class="sort__item-star-number">
+                        (<p>0</p>)
+                    </div>
                 </div>
             </div>
-            <div class="sort__item-star">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <div class="sort__item-star-number">
-                    (<p>0</p>)</div>
-            </div>
         </div>
-    </div>
-</div>`
+    </div>`
     var sort__item_btn_left = $('.sort__item-img_btn-left')
     var sort__item_btn_right = $('.sort__item-img_btn-right')
     var list_itemLength = list_item[i].img.length
